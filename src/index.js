91688e6a8755997e2b339cc23827ff6aff7530b9
ddmng <daniele.diminica@gmail.com>
2018-07-04 00:14:49 +0200

    Added example with boolean in state

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import 'bootstrap'
 import './scss/index.scss'
 
 const state = {
-  count: 0
+  count: 0,
+  globalBoolean: false
 }
 
 const actions = {
   down: value => state => ({ count: state.count - value }),
-  up: value => state => ({ count: state.count + value })
+  up: value => state => ({ count: state.count + value }),
+  toggle: value => state => ({ globalBoolean: !state.globalBoolean })
 }
 
 const view = (state, actions) => (
@@ -16,6 +18,8 @@ const view = (state, actions) => (
     <h1>{state.count}</h1>
     <button class="btn btn-primary" onclick={() => actions.down(1)}>-</button>
     <button class="btn btn-warning" onclick={() => actions.up(3)}>+</button>
+    <button class="btn btn-default" onclick={() => actions.toggle()}>toggle</button>
+    <h2>{ state.globalBoolean?"True":"False" }</h2>
   </div>
 )
 
