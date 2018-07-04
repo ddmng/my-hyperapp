26f5981fc17b50cc9f4862cca4450d19c2ced8d7
ddmng <daniele.diminica@gmail.com>
2018-07-05 00:17:30 +0200

    Some step forward

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
 import { h, app } from "hyperapp"
 import 'bootstrap'
 import './scss/index.scss'
+import { List } from 'immutable'
 
 const state = {
-  count: 0,
-  globalBoolean: false
+  todos: List([]),
+  entering: ""
 }
 
 const actions = {
-  down: value => state => ({ count: state.count - value }),
-  up: value => state => ({ count: state.count + value }),
-  toggle: value => state => ({ globalBoolean: !state.globalBoolean })
+  add: value => state => {
+    console.log("prev state: ", state)
+    console.log("cur value: ", value)
+    return ({ todos: state.todos.insert(0, value), entering: "" })
+  },
+  updateEntering: value => state => {
+    console.log("adding: ", value)
+    return ({ todos: state.todos, entering: value })
+  }
 }
 
 const view = (state, actions) => (
   <div>
-    <h1>{state.count}</h1>
-    <button class="btn btn-primary" onclick={() => actions.down(1)}><span class="fa fa-minus"></span></button>
-    <button class="btn btn-warning" onclick={() => actions.up(3)}><span class="fa fa-plus"></span></button>
-    <button class="btn btn-default" onclick={() => actions.toggle()}><span class="fa fa-stop"></span></button>
-    <h2>{ state.globalBoolean?"True":"False" }</h2>
+    <div>
+      <input type="text" class="" onchange={(e) => actions.updateEntering(e.target.value)} value={state.entering}></input>
+      <button class="btn btn-default" onclick={() => actions.add({ id: 999, text: state.entering, done: false })}> <span class="fa fa-plus"></span> </button>
+    </div>
+    <div>
+      <h1>Todo list</h1>
+      <ul>
+        { state.todos.map( (x) => <li>testo</li>) }
+      </ul>
+    </div>
   </div>
 )
 
