8ee9bf0a84beaed75f3b34eb72c37c92cd9af165
ddmng <daniele.diminica@gmail.com>
2018-07-06 10:25:49 +0200

    Add login Component

--- src/index.js
+++ src/index.js
@@ -4,28 +4,46 @@ import "./scss/index.scss";
 
 const state = {
   todos: [],
-  entering: ""
+  entering: "",
+  auth: {
+    loggedin: false,
+    loginError: "",
+    user: ""
+  }
+};
+
+/**
+ * Backend URLs
+ */
+const urls = {
+  login: "http://localhost:4000/api/users/sign_in"
 }
 
+/**
+ * Hyperapp Actions
+ */
 const actions = {
+  // TODO move this code outside action
   add: value => state => {
-    if(!value)
-      return state
-    const elem = { 
+    if (!value) return state;
+    const elem = {
       id: uniqueId(state.todos),
       value: value,
       done: false
-    }
+    };
 
-    return { todos: state.todos.concat(elem), entering: "" };
+    return {...state, todos: state.todos.concat(elem), entering: "" };
   },
   updateEntering: value => state => {
-    return { todos: state.todos, entering: value };
+    return {...state, entering: value };
   },
   delete: value => state => {
-    return { todos: state.todos.filter(el => el.id != value) };
+    return {...state,  todos: state.todos.filter(el => el.id != value) };
+  },
+  loginSuccess: value => state => {
+    return {...state, auth: {...state.auth, loggedin: true} }
   }
-}
+};
 
 /**
  * Returns a new uniqueId from the todos list picking the max id value from the array
@@ -39,34 +57,42 @@ const uniqueId = todos => {
   } else {
     return 0;
   }
-}
+};
 
-export const TodoItem =  ({ id, value, done, toggle }) => (
+export const TodoItem = ({ id, value, done, toggle }) => (
   <li>
     {value}
     <button class="btn btn-danger" onclick={() => toggle(id)}>
       <span class="fa fa-minus" />
     </button>
   </li>
-)
+);
 
 export const TodoList = () => (state, actions) => (
   <div>
-  <h1>Todo list</h1>
-  <ul>
-    {state.todos.map(x => (
-      <TodoItem
-        id={x.id}
-        value={x.value}
-        done={x.done}
-        toggle={actions.delete}
-      />
-    ))}
-  </ul>
+    <h1>Todo list</h1>
+    <ul>
+      {state.todos.map(x => (
+        <TodoItem
+          id={x.id}
+          value={x.value}
+          done={x.done}
+          toggle={actions.delete}
+        />
+      ))}
+    </ul>
   </div>
-)
+);
 
-const view = (state, actions) => (
+const LoginDialog = () => (state, actions) => (
+  <div>
+    <input type="text" value="" />
+    <input type="text" value="" />
+    <button class="btn btn-primary" onclick={ () => actions.loginSuccess() } >Login</button>
+  </div>
+);
+
+const MainPage = () => (state, actions) => (
   <div>
     <div>
       <input
@@ -77,15 +103,21 @@ const view = (state, actions) => (
       />
       <button
         class="btn btn-default"
-        onclick={() =>
-          actions.add(state.entering)
-        }
+        onclick={() => actions.add(state.entering)}
       >
         <span class="fa fa-plus" />
       </button>
     </div>
-        <TodoList />
+    <TodoList />
   </div>
 );
+
+const view = (state, actions) => {
+  if (state.auth.loggedin) {
+    return <div><MainPage /></div>
+  } else {
+    return <div><LoginDialog /></div>
+  }
+};
 
 app(state, actions, view, document.getElementById("app"));
