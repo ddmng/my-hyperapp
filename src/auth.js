b9f3f34925c0c41a4ec2fde9cb39d557376e082d
ddmng <daniele.diminica@gmail.com>
2018-07-06 13:20:32 +0200

    login sty;le

--- src/auth.js
+++ src/auth.js
@@ -6,10 +6,14 @@ import { actions } from "./state/actions"
  * Login dialog Component
  */
 export const LoginDialog = () => (state, actions) => (
-    <div>
-      <input type="text" value={state.auth.email} onchange={(e) => actions.auth.updateEmail(e.target.value)} />
-      <input type="text" value={state.auth.password} onchange={(e) => actions.auth.updatePassword(e.target.value)} />
-      <button class="btn btn-primary" onclick={ () => actions.auth.login({email: state.auth.email, password: state.auth.password}) } >Login</button>
+  <div class="row">
+    <div class="col">
+      <input type="email" class="form-control" value={state.auth.email} onchange={(e) => actions.auth.updateEmail(e.target.value)} />
     </div>
-  );
+    <div class="col">
+      <input type="password" class="form-control" value={state.auth.password} onchange={(e) => actions.auth.updatePassword(e.target.value)} />
+    </div>
+    <button class="btn btn-primary" onclick={ () => actions.auth.login({email: state.auth.email, password: state.auth.password}) } >Login</button>
+  </div>
+);
   
