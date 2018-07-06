1e0718470f4b40d3bed7f19a85b1278b94f55a6e
ddmng <daniele.diminica@gmail.com>
2018-07-06 17:36:55 +0200

    Fix layout

--- src/layout/login.js
+++ src/layout/login.js
@@ -1,30 +1,42 @@
 import { h } from "hyperapp";
-import { state } from "../state/state.js"
-import { actions } from "../state/actions.js"
 
 /**
  * Login dialog Component
  */
 export const LoginDialog = () => (state, actions) => (
-  
-  <div class="form-inline">
-    <div class="form-group mb-2">
-        <div class="col">
-        <input type="email" 
-                class="form-control" 
-                value={state.auth.email} 
-                onchange={(e) => actions.auth.updateEmail(e.target.value)} />
+  <div class="container">
+    <div class="row">
+      <h1>&nbsp;</h1>
+    </div>
+    <div class="jumbotron">
+    <div class="row">
+      <h1 class="display-3 mx-auto">M1 Monitor</h1>
+    </div>
+    <div class="row">
+      <h1>&nbsp;</h1>
+    </div>
+      <div class="row">
+        <div class="form-inline mx-auto">
+          <div class="form-group mb-2">
+              <div class="col">
+              <input type="email" 
+                      class="form-control" 
+                      value={state.auth.email} 
+                      onchange={(e) => actions.auth.updateEmail(e.target.value)} />
+            </div>
+            <div class="col">
+              <input type="password" 
+                      class="form-control" 
+                      value={state.auth.password} 
+                      onchange={(e) => actions.auth.updatePassword(e.target.value)} />
+            </div>
+            <button class="btn btn-primary" disabled={state.auth.loggingIn} 
+                    onclick={ () => actions.auth.login({email: state.auth.email, password: state.auth.password}) } >
+                    <i class={state.auth.loggingIn ? "fa fa-spinner fa-spin" : "fa fa-sign-in"}></i> Login
+            </button>
+          </div>
+        </div>
       </div>
-      <div class="col">
-        <input type="password" 
-                class="form-control" 
-                value={state.auth.password} 
-                onchange={(e) => actions.auth.updatePassword(e.target.value)} />
-      </div>
-      <button class="btn btn-primary" disabled={state.auth.loggingIn} 
-              onclick={ () => actions.auth.login({email: state.auth.email, password: state.auth.password}) } >
-              <i class={state.auth.loggingIn ? "fa fa-spinner fa-spin" : "fa fa-sign-in"}></i> Login
-      </button>
     </div>
   </div>
 );
